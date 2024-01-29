175b21183f6cda9d48dee512165bba994a691d93
mike-westaway <miwestaw@microsoft.com>
2024-01-29 17:24:17 +0000

    Update locals-naming.tf
    added app_service_random_id

--- Modules/Core/linux_web_app/modules/linux-web-app/locals-naming.tf
+++ Modules/Core/linux_web_app/modules/linux-web-app/locals-naming.tf
@@ -3,7 +3,8 @@ locals {
   name_prefix = lower(var.name_prefix)
   name_suffix = lower(var.name_suffix)
 
-  app_service_name  = coalesce(var.app_service_custom_name, azurecaf_name.app_service_web.result)
+  app_service_name  = coalesce(var.app_service_custom_name, azurecaf_name.app_service_web.result, var.app_service_random_id)
+
   staging_slot_name = coalesce(var.staging_slot_custom_name, "staging-slot")
 
   app_insights_name = coalesce(var.application_insights_custom_name, azurecaf_name.application_insights.result)
